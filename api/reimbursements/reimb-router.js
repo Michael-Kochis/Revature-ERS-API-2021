cdc2919f56d55a09d29c7dbffd01ef905eb6709e
Michael-Kochis <rhishisikk@gmail.com>
2021-09-27 11:39:52 -0500

    Uncomment the remaining reimb GETs

--- api/reimbursements/reimb-router.js
+++ api/reimbursements/reimb-router.js
@@ -10,22 +10,22 @@ router.get('/', (req, res, next) => {
     }).catch(next);
 })
 
-// router.get('/user/:USER_ID', (req, res, next) => {
-//     const { USER_ID } = req.params;
+router.get('/user/:USER_ID', (req, res, next) => {
+    const { USER_ID } = req.params;
     
-//     reimb.findReimbByUserID(USER_ID)
-//     .then(resp => {
-//         res.status(200).json(resp);
-//     }).catch(next);
-// })
+    reimb.findReimbByUserID(USER_ID)
+    .then(resp => {
+        res.status(200).json(resp);
+    }).catch(next);
+})
 
-// router.get('/status/:reimb_status', (req, res, next) => {
-//     const { reimb_status } = req.params;
+router.get('/status/:reimb_status', (req, res, next) => {
+    const { reimb_status } = req.params;
     
-//     reimb.findReimbByStatus(reimb_status)
-//     .then(resp => {
-//         res.status(200).json(resp);
-//     }).catch(next);
-// })
+    reimb.findReimbByStatus(reimb_status)
+    .then(resp => {
+        res.status(200).json(resp);
+    }).catch(next);
+})
 
 module.exports = router;
